853009ac44c4d2cf1e2096f069f7e40e331171bf
Mike Fougere <fougere.mike@gmail.com>
2018-06-27 14:46:14 -0400

    Publish extensions to artifactory.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 LIBRARIES="core dash hls smoothstreaming ui"
+EXTENSIONS="cast gvr ima jobdispatcher leanback mediasession okhttp rtmp"
 
 ./gradlew clean assemble
 
+./gradlew "library:artifactoryPublish"
 for lib in $LIBRARIES; do
+		echo "------------------------------------------------------------------------"
+		echo "Publishing library-$lib"
+		echo
+
 		./gradlew "library-$lib:artifactoryPublish"
+done
+
+for ext in $EXTENSIONS; do
+		echo "------------------------------------------------------------------------"
+		echo "Publishing extension-$ext"
+		echo
+
+		./gradlew "extension-$ext:artifactoryPublish"
 done
